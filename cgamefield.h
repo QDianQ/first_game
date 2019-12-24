5df6dcd38e8b5d5f5463e671b6a2ea0e1fc24b52
Aleksandr Gorbachev <gorb.htb@gmail.com>
2019-12-25 00:23:38 +0300

    Пояснения по работе функции перемешивания массива

--- cgamefield.h
+++ cgamefield.h
@@ -5,14 +5,20 @@
 class CGameField
 {
 public:
-    int N,*buf;
+    // * Это стоит убрать в protected
+    // >-----------------------------
+    int N, *buf;
     int **mass;
+    // ----------------------------->
 
     CGameField(int n = 0);
     ~CGameField();
     void create_field();
     void show_field();
     void random_field();
+
+    // * Нужна функция приводящая mass к виду buf и наоборот
+    // * Будет полезно для проверки на победу
 };
 
 #endif // CGAMEFIELD_H
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 
     CGameField *game = new CGameField;    //Размер матрицы
 
-    game->create_field();
+    game->create_field(); // * сейчас это не нужно
  //   game->show_field();
 
 //    for ( int i = 0; i < game->N; i++){       //Вывод матрицы на экран в понятном виде для пользователя
@@ -19,6 +19,8 @@ int main()
 //    }
 
     //вызов функции (ради проверки работает ли она как надо или нет)
+    // * В последствии вызов этой функции нужно убрать в класс CGameField
     f_rand(game->buf,game->N);
+    // ! game->buf не создан вообще !
     return 0;
 }
