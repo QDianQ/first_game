bf2bd212cc04a6722e185e69762c1cefdeedf880
Vlad <none.@mail.com>
2020-01-06 16:17:51 +0300

    объеденил мердж и свои наработки

---
+++ func_check_win.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "func_check_win.h"
+
+bool f_check_win(int *mass_converted, int size)  //функция проверки условаия победы
+{
+    int count=0;    //счетчик кол-ва правильных цифр в массиве
+    for(int i=0;i<size*size;i++){
+        if(mass_converted[i]==i)
+            count++;
+    }
+    if (count==size*size){  //если кол-во правильных цифр совпадает с размером массива
+        return 1;           // то вернуть true
+    }else{                  //иначе
+        return 0;           //вернуть false
+    }
+}
--- func_check_win.h
+++ func_check_win.h
@@ -1,4 +1,6 @@
 #ifndef FUNC_CHECK_WIN_H
 #define FUNC_CHECK_WIN_H
 
+bool f_check_win(int *mass_converted, int size);
+
 #endif // FUNC_CHECK_WIN_H
